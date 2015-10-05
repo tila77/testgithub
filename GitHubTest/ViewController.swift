46338d8ceb4a23c881caa1d5c60a77cba660d071
Tino Langer <Tino@tinosmacbook.fritz.box>
2015-10-05 20:57:57 +0200

    Initial Commit

---
+++ GitHubTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHubTest
+//
+//  Created by Tino Langer on 05.10.15.
+//  Copyright © 2015 Tino Langer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
