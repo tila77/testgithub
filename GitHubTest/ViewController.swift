e2d8743a5f1f636b36fd73b99d7be444892d0cd9
Tino Langer <Tino@tinosmacbookpro.local>
2015-10-06 13:31:33 +0200

    edwdas

--- GitHubTest/ViewController.swift
+++ GitHubTest/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("test123")
+        print("test1231234")
     }
 
     override func didReceiveMemoryWarning() {
